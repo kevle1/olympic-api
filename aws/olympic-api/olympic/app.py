e4687d5392dbc02d0255525260e01945c1641c92
Jonathan Wright <37826825+Techypanda@users.noreply.github.com>
2021-07-25 16:54:08 +0800

    cors
    fix cors i have not tested :)

--- aws/olympic-api/olympic/app.py
+++ aws/olympic-api/olympic/app.py
@@ -61,6 +61,11 @@ def lambda_handler(event, context):
     
     return {
         "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,X-Amz-Security-Token,Authorization,X-Api-Key,X-Requested-With,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers",
+		    "Access-Control-Allow-Origin":  "*",
+			"Access-Control-Allow-Methods": "GET"
+        },
         "body": json.dumps(rankings),
         "isBase64Encoded": False
     }
